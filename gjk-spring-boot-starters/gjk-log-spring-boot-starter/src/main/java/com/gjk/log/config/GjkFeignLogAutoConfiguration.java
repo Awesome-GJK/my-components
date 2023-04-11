73a9bbbef5c8a330335a44a0c582a7346297ae68
gaojiankang <gaojiankang@ykccn.com>
2023-04-11 17:55:45 +0800

    添加注入条件

--- gjk-spring-boot-starters/gjk-log-spring-boot-starter/src/main/java/com/gjk/log/config/GjkFeignLogAutoConfiguration.java
+++ gjk-spring-boot-starters/gjk-log-spring-boot-starter/src/main/java/com/gjk/log/config/GjkFeignLogAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.gjk.log.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.FeignLoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ import com.gjk.log.feign.GjkFeignLoggerFactory;
 public class GjkFeignLogAutoConfiguration {
 
     @Bean
+    @ConditionalOnProperty(name = "gjk.log.feign.enable", havingValue = "true")
     public FeignLoggerFactory orderFeignLoggerFactory(LogProperties logProperties) {
         return new GjkFeignLoggerFactory(logProperties);
     }
